fae4ec0a3b779e061dc47d938b9dff127d3b8ba8
Super_QB <bluemagicalwolf@gmail.com>
2021-08-06 17:03:51 +0200

    Fixed a stupid mistake

--- components/8x-multiplexer.bat
+++ components/8x-multiplexer.bat
@@ -1,4 +1,6 @@
 @echo off
+color a
+
 echo enter value for A0 (0 or 1)
 set /p a0=
 echo enter value for A1 (0 or 1)
@@ -48,65 +50,64 @@ echo.
 
 echo inverter connected to sel for and1 > .nul
 
-IF /I "%sel%" == "1" (set /a "selinv=0") ELSE (set /a "selinv=1")
-
+IF /I %sel% == 1 (set /a "selinv=0") ELSE (set /a "selinv=1")
 
 echo and 1 connected to A and inverted Sel > .nul
 
-set /a 0and1=a0+selinv
-IF /I "%0and1%" == "2" (set /a "0o1=1") ELSE (set /a "0o1=0")
-set /a 1and1=a1+selinv
-IF /I "%1and1%" == "2" (set /a "1o1=1") ELSE (set /a "1o1=0")
-set /a 2and1=a2+selinv
-IF /I "%2and1%" == "2" (set /a "2o1=1") ELSE (set /a "2o1=0")
-set /a 3and1=a3+selinv
-IF /I "%3and1%" == "2" (set /a "3o1=1") ELSE (set /a "3o1=0")
-set /a 4and1=a4+selinv
-IF /I "%4and1%" == "2" (set /a "4o1=1") ELSE (set /a "4o1=0")
-set /a 5and1=a5+selinv
-IF /I "%5and1%" == "2" (set /a "5o1=1") ELSE (set /a "5o1=0")
-set /a 6and1=a6+selinv
-IF /I "%6and1%" == "2" (set /a "6o1=1") ELSE (set /a "6o1=0")
-set /a 7and1=a7+selinv
-IF /I "%7and1%" == "2" (set /a "7o1=1") ELSE (set /a "7o1=0")
+set /a Aand1=a0+selinv
+IF /I %Aand1% == 2 (set /a "Ao1=1") ELSE (set /a "Ao1=0")
+set /a Band1=a1+selinv
+IF /I %Band1% == 2 (set /a "Bo1=1") ELSE (set /a "Bo1=0")
+set /a Cand1=a2+selinv
+IF /I %Cand1% == 2 (set /a "Co1=1") ELSE (set /a "Co1=0")
+set /a Dand1=a3+selinv
+IF /I %Dand1% == 2 (set /a "Do1=1") ELSE (set /a "Do1=0")
+set /a Eand1=a4+selinv
+IF /I %Eand1% == 2 (set /a "Eo1=1") ELSE (set /a "Eo1=0")
+set /a Fand1=a5+selinv
+IF /I %Fand1% == 2 (set /a "Fo1=1") ELSE (set /a "Fo1=0")
+set /a Gand1=a6+selinv
+IF /I %Gand1% == 2 (set /a "Go1=1") ELSE (set /a "Go1=0")
+set /a Hand1=a7+selinv
+IF /I %Hand1% == 2 (set /a "Ho1=1") ELSE (set /a "Ho1=0")
 
 echo and2 connected to B and Sel > .nul
 
-set /a 0and2=sel+b0
-IF /I "%0and2%" == "2" (set /a "0o2=1") ELSE (set /a "0o2=0")
-set /a 1and2=sel+b1
-IF /I "%1and2%" == "2" (set /a "1o2=1") ELSE (set /a "1o2=0")
-set /a 2and2=sel+b2
-IF /I "%2and2%" == "2" (set /a "2o2=1") ELSE (set /a "2o2=0")
-set /a 3and2=sel+b3
-IF /I "%3and2%" == "2" (set /a "3o2=1") ELSE (set /a "3o2=0")
-set /a 4and2=sel+b4
-IF /I "%4and2%" == "2" (set /a "4o2=1") ELSE (set /a "4o2=0")
-set /a 5and2=sel+b5
-IF /I "%5and2%" == "2" (set /a "5o2=1") ELSE (set /a "5o2=0")
-set /a 6and2=sel+b6
-IF /I "%6and2%" == "2" (set /a "6o2=1") ELSE (set /a "6o2=0")
-set /a 7and2=sel+b7
-IF /I "%7and2%" == "2" (set /a "7o2=1") ELSE (set /a "7o2=0")
+set /a Aand2=sel+b0
+IF /I %Aand2% == 2 (set /a "Ao2=1") ELSE (set /a "Ao2=0")
+set /a Band2=sel+b1
+IF /I %Band2% == 2 (set /a "Bo2=1") ELSE (set /a "Bo2=0")
+set /a Cand2=sel+b2
+IF /I %Cand2% == 2 (set /a "Co2=1") ELSE (set /a "Co2=0")
+set /a Dand2=sel+b3
+IF /I %Dand2% == 2 (set /a "Do2=1") ELSE (set /a "Do2=0")
+set /a Eand2=sel+b4
+IF /I %Eand2% == 2 (set /a "Eo2=1") ELSE (set /a "Eo2=0")
+set /a Fand2=sel+b5
+IF /I %Fand2% == 2 (set /a "Fo2=1") ELSE (set /a "Fo2=0")
+set /a Gand2=sel+b6
+IF /I %Gand2% == 2 (set /a "Go2=1") ELSE (set /a "Go2=0")
+set /a Hand2=sel+b7
+IF /I %Hand2% == 2 (set /a "Ho2=1") ELSE (set /a "Ho2=0")
 
 echo or1 connected to O1 and O2 > .nul
 
-set /a 0o3=0o1+0o2
-IF /I "%0o3%" == "0" (set /a "z0=0") ELSE (set /a "z0=1")
-set /a 1o3=1o1+1o2
-IF /I "%1o3%" == "0" (set /a "z1=0") ELSE (set /a "z1=1")
-set /a 2o3=2o1+2o2
-IF /I "%2o3%" == "0" (set /a "z2=0") ELSE (set /a "z2=1")
-set /a 3o3=3o1+3o2
-IF /I "%3o3%" == "0" (set /a "z3=0") ELSE (set /a "z3=1")
-set /a 4o3=4o1+4o2
-IF /I "%4o3%" == "0" (set /a "z4=0") ELSE (set /a "z4=1")
-set /a 5o3=5o1+5o2
-IF /I "%5o3%" == "0" (set /a "z5=0") ELSE (set /a "z5=1")
-set /a 6o3=6o1+6o2
-IF /I "%6o3%" == "0" (set /a "z6=0") ELSE (set /a "z6=1")
-set /a 7o3=7o1+7o2
-IF /I "%7o3%" == "0" (set /a "z7=0") ELSE (set /a "z7=1")
+set /a Ao3=Ao1+Ao2
+IF /I %Ao3% == 0 (set /a "z0=0") ELSE (set /a "z0=1")
+set /a Bo3=Bo1+Bo2
+IF /I %Bo3% == 0 (set /a "z1=0") ELSE (set /a "z1=1")
+set /a Co3=Co1+Co2
+IF /I %Co3% == 0 (set /a "z2=0") ELSE (set /a "z2=1")
+set /a Do3=Do1+Do2
+IF /I %Do3% == 0 (set /a "z3=0") ELSE (set /a "z3=1")
+set /a Eo3=Eo1+Eo2
+IF /I %Eo3% == 0 (set /a "z4=0") ELSE (set /a "z4=1")
+set /a Fo3=Fo1+Fo2
+IF /I %Fo3% == 0 (set /a "z5=0") ELSE (set /a "z5=1")
+set /a Go3=Go1+Go2
+IF /I %Go3% == 0 (set /a "z6=0") ELSE (set /a "z6=1")
+set /a Ho3=Ho1+Ho2
+IF /I %Ho3% == 0 (set /a "z7=0") ELSE (set /a "z7=1")
 
 echo Z0=%z0% Z1=%z1% 
 echo Z2=%z2% Z3=%z3% 
