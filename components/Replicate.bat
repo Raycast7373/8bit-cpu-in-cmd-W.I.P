11707236796d7aa42ed54c1f767ccade01401f79
Super_QB <bluemagicalwolf@gmail.com>
2021-08-07 17:20:45 +0200

    Add files via upload

---
+++ components/Replicate.bat
@@ -0,0 +1,23 @@
+@echo off
+color a
+echo Please enter a value for A (1 or 0)
+set /p i=
+echo.
+echo Current value:
+echo A=%i%
+echo.
+set ze0=%i%
+set ze1=%i%
+set ze2=%i%
+set ze3=%i%
+set ze4=%i%
+set ze5=%i%
+set ze6=%i%
+set ze7=%i%
+echo Input:
+echo A=%i%
+echo.
+echo Output:
+echo ZE0 to ZE7= %ze0%
+echo.
+pause
